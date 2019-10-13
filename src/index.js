2cc3491d5b00f708c541652a9ecc8671550ed3f0
Nataly <natalie.smirnowa@gmail.com>
2019-10-13 16:11:13 +0300

    feat: add task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+    let map = new Map();
+    for (let i = 0; i < bracketsConfig.length; i++) {
+        let element = bracketsConfig[i];
+        map.set(element[1], element[0])
+    }
+    let stack = [];
+    for (let i = 0; i < str.length; i++) {
+        let element = str[i];
+        let values = Array.from(map.values());
+        if (values.includes(element)) {
+            if (element === map.get(element)) {
+                let lastElement = stack.pop();
+                if (lastElement !== element) {
+                    if (lastElement) {
+                        stack.push(lastElement);
+                    }
+                    stack.push(element);
+                }
+            } else {
+                stack.push(element);
+            }
+        } else {
+            let lastElement = stack.pop();
+            let correctBracket = map.get(element);
+            if (lastElement !== correctBracket) {
+                return false;
+            }
+        }
+
+    }
+    return stack.length ? false : true;
 }
